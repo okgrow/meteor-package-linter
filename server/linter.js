77f3a3b527be913e2c1a10de8c2e421ebf092ad3
Dean Radcliffe <deanmisc@yahoo.com>
2015-09-22 14:47:49 -0500

    new rule for deprecated packages

--- server/linter.js
+++ server/linter.js
@@ -10,8 +10,14 @@ PackageLinter = {
   latestMeteorVersionOfPackage: latestMeteorVersionOfPackage
 };
 
+var _deprecationMap = {
+  "okgrow:iron-router-autoscroll": "okgrow:router-autoscroll",
+  "meteorhacks:flow-router": "kadira:flow-router",
+  "deanius:promise": "okgrow:promise"
+};
+
 var lintRules = [
-    function (packageModel) {
+    function DepsShouldDeclareVersion (packageModel) {
       var code = "2.1",
         severity=1,
         error="External dependencies should declare a version",
@@ -20,6 +26,21 @@ var lintRules = [
       for (var p in packageModel.externalDeps.uses) {
         var dep = packageModel.externalDeps.uses[p];
         if (!dep.versionNum) {
+          ruleErrs.push({code: code, severity: severity, offender: p, error: error, details: dep})
+        }
+      }
+
+      return ruleErrs;
+    },
+    function DepsShouldNotReferToDeprecated (packageModel) {
+      var code = "2.2",
+        severity=1,
+        error="Dependencies should not refer to deprecated versions.",
+        ruleErrs = [];
+
+      for (var p in packageModel.externalDeps.uses) {
+        var dep = packageModel.externalDeps.uses[p];
+        if( _deprecationMap[p] ){
           ruleErrs.push({code: code, severity: severity, offender: p, error: error, details: dep})
         }
       }
